70a1234f965d277a8b1c16a3ba4e62354076edb9
Gabriel <gabriel.pro.d3@gmail.com>
2019-02-19 22:49:01 +0100

    fix

--- src/js/core/router/Instance.js
+++ src/js/core/router/Instance.js
@@ -1,4 +1,4 @@
 export let instances = []
 
-export const register = (comp) => instances.push(comp)
-export const unregister = (comp) => instances.splice(instances.indexOf(comp),
+export const register = (comp) => instances.push(comp);
+export const unregister = (comp) => instances.splice(instances.indexOf(comp);
